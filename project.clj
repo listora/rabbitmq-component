41558c68ef5f0c65dbc1a246724917a491e3881d
James Reeves <jreeves@weavejester.com>
2014-10-08 17:54:47 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject listora/rabbitmq-component "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A component for managing a connection to RabbitMQ"
+  :url "https://github.com/listora/rabbitmq-component"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.novemberain/langohr "2.11.0"]
