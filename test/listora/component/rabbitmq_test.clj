3bfc3597f3e2187c579cabce36398eafb0947524
James Reeves <jreeves@weavejester.com>
2014-10-08 16:16:01 +0100

    Initial commit

---
+++ test/listora/component/rabbitmq_test.clj
@@ -0,0 +1,7 @@
+(ns listora.component.rabbitmq-test
+  (:require [clojure.test :refer :all]
+            [listora.component.rabbitmq :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
